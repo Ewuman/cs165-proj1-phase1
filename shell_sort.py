777e7dad211de2de2af7390da5668fd88253c2e9
Ethan Wu <wueh2@uci.edu>
2026-04-18 18:44:51 -0700

    implement shell_sort2

--- shell_sort.py
+++ shell_sort.py
@@ -21,3 +21,35 @@ def shell_sort1(nums: list[int]):
         gap //= 2
     
     return num_comparisons
+
+
+def shell_sort2(nums: list[int]):
+    num_comparisons = 0
+    n = len(nums)
+
+    k = 1
+    prev_gap = None
+    while True:
+        gap = 2 * (n // (2 ** (k+1))) + 1
+        if gap <= 0 or gap == prev_gap:
+            break
+
+        prev_gap = gap
+
+        for i in range(gap, n):
+            temp = nums[i]
+            j = i
+
+            while j >= gap and nums[j-gap] > temp:
+                num_comparisons += 1
+                nums[j] = nums[j-gap]
+                j -= gap
+
+            if j >= gap:
+                num_comparisons += 1
+
+            nums[j] = temp
+
+        k += 1
+    
+    return num_comparisons
